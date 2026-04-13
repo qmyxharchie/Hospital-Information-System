d56465509371bcba8e58e7b0c526f5e437a38a05
qmyxharchie <13943820020@163.com>
2026-04-13 15:11:58 +0800

    新增了patient.c的添加病人函数

--- src/patient.c
+++ src/patient.c
@@ -9,10 +9,32 @@
 
 //--------------------
 //以下为添加病人函数
-void addPatient(Patient* head, Patient* tail,
+void addPatient(Patient** head, Patient** tail,
     char name[], char gender[], char idCard[], char phone[]) {
+    Patient* newNode = (Patient*)malloc(sizeof(Patient));
+    char id[20];
+    generateUniqueId("PT", id);
+    strcpy(newNode->data.cardNo, id);
+    strcpy(newNode->data.name, name);
+    strcpy(newNode->data.gender, gender);
+    strcpy(newNode->data.idCard, idCard);
+    strcpy(newNode->data.phone, phone);
+    node->data.isActive = 0;
+    newNode->next = newNode->pre = NULL;
+    if (*tail == NULL) {
+        *head = newNode;
+        *tail = newNode;
+    }
+    else {
+        (*tail)->next = newNode;
+        newNode->pre = *tail;
+        *tail = newNode;
+    }
+    rebuildPatientFile(*head);
+    printf("添加成功,卡号为%c", id);
 
 }
+
 void delPatient(Patient* head, Patient* tail, PatientData p)    //删除病人
 void modifyPatient(Patient* p, char phone[]);                   //修改病人信息
 Patient* findPatientByCardNo(Patient* head, char* cardNo);      //按卡号精确查找
